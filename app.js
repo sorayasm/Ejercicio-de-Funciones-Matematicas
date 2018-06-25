b98a96a696ff1374a5c4a6d1ab726d384dc1ba62
SorayaSM <soraya.sacaan@gmail.com>
2018-06-25 16:16:52 -0400

    actualizando el ejercicio

---
+++ app.js
@@ -0,0 +1,11 @@
+function add() {
+    const n1 = parseInt(document.getElementById("Numb1").value);
+    const n2 = parseInt(document.getElementById("Numb2").value);
+    console.log(n1 + n2);
+}
+
+function multiply() {
+    const n3 = parseInt(document.getElementById("Numb3").value);
+    const n4 = parseInt(document.getElementById("Numb4").value);
+    console.log(n3 * n4);
+}
